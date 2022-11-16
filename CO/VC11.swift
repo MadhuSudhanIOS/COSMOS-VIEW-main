77b820406864dac900e4e89fda2ff50956ee3224
IOS <medicoverhospitalsios@gmail.com>
2022-11-16 17:22:25 +0530

    Added VC11

---
+++ CO/VC11.swift
@@ -0,0 +1,29 @@
+//
+//  VC11.swift
+//  CO
+//
+//  Created by Rajesh Parimala on 16/11/22.
+//
+
+import UIKit
+
+class VC11: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
